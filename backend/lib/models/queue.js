096cfbfc58245c48c5340bf1deac5ab92e470811
Joseph Chen <jchen42703@gmail.com>
2022-01-20 00:55:20 -0500

    Correct collection name for queue schema/model

--- backend/lib/models/queue.js
+++ backend/lib/models/queue.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const { userSchema } = require("./user");
 
-const queueSchema = new mongoose.Schema({
-  queueId: String,
-  adminId: String,
-  queue: [userSchema],
-});
+const queueSchema = new mongoose.Schema(
+  {
+    queueId: String,
+    adminId: String,
+    queue: [userSchema],
+  },
+  { collection: "queues" }
+);
 
-const Queue = mongoose.model("User", queueSchema);
+const Queue = mongoose.model("Queue", queueSchema);
 module.exports = {
   Queue,
 };
