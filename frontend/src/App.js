895fbb3c1a02a3dc7ad84d4bb9356da9895ee611
Joseph Chen <jchen42703@gmail.com>
2022-01-29 15:52:12 -0500

    basic frontend conditional render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const StatusMessage = (props) => {
+  // props: {
+  //   status: boolean
+  // }
+  if (props.statusValue) {
+    return <p>Success!</p>;
+  } else {
+    return <p>Error :(</p>;
+  }
+};
+
+const App = () => {
+  const [status, setStatus] = useState(false);
+
+  const handleClick = async () => {
+    const payload = {
+      adminId: "testAdmin",
+    };
+    try {
+      const data = await postData(
+        "http://localhost:5000/api/queue/create",
+        payload
+      );
+      console.log("data: ", data);
+      setStatus(true);
+    } catch (err) {
+      console.log("err: ", err);
+      setStatus(false);
+    }
+  };
+
+  const postData = async (url = "", data = {}) => {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Create queue</button>
+      <p>Status: {JSON.stringify(status)}</p>
+      <StatusMessage statusValue={status}></StatusMessage>
     </div>
   );
-}
+};
 
 export default App;
