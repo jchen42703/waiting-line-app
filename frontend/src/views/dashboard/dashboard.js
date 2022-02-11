7d763a456bdeedf6668c9c05cd54c748870f1b81
Joseph Chen <jchen42703@gmail.com>
2022-02-11 13:59:39 -0500

    Added daisyui

--- frontend/src/views/dashboard/dashboard.js
+++ frontend/src/views/dashboard/dashboard.js
@@ -2,6 +2,7 @@ export default function Dashboard() {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Dashboard</h2>
+      <button class="btn btn-primary">daisyUI Button</button>
     </main>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
