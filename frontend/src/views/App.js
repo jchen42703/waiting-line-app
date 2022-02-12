cb202f3e6b010925fdd734f69d475528b47656a8
Joseph Chen <jchen42703@gmail.com>
2022-02-12 01:48:19 -0500

    global navbar and added route for settings

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./auth/Login";
 import Dashboard from "./dashboard/dashboard";
 import Home from "./home/Home";
+import SettingsPage from "./settings/settings";
 import { ChakraProvider } from "@chakra-ui/react";
+import Nav from "../components/NavBar";
 
 export default function App() {
   return (
     <ChakraProvider resetCSS={true}>
       <BrowserRouter>
+        <Nav></Nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
+          <Route path="settings" element={<SettingsPage />} />
         </Routes>{" "}
       </BrowserRouter>
     </ChakraProvider>
--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -1,12 +1,8 @@
 import "../../styles/styles.scss";
-import Nav from "../../components/NavBar";
-import NavDrawer from "../../components/Drawer";
 
 export default function Home() {
   return (
     <div>
-      <Nav></Nav>
-      <NavDrawer></NavDrawer>
       <h1 className="text-3xl font-bold underline"> Hello world!</h1>
     </div>
   );
