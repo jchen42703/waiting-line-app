ac16d96e137e1ce7e2886eafa72a4c7264a910e3
Joseph Chen <jchen42703@gmail.com>
2022-02-20 16:43:52 -0500

    Basic template for the Queue Dashboard page
    - Created `QueueDashboard`
    - Added the `/dashboard/:queueId` route
    - Was done in conjuction with @Azuzota

--- packages/frontend/src/views/App.jsx
+++ packages/frontend/src/views/App.jsx
@@ -1,11 +1,12 @@
 import "../styles/styles.scss";
 import "focus-visible/dist/focus-visible";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./home/Home";
 import Login from "./auth/Login";
 import Dashboard from "./dashboard/dashboard";
-import Home from "./home/Home";
+import QueueDashboard from "./dashboard/queueDashboard";
 import SettingsPage from "./settings/settings";
-import { ChakraProvider } from "@chakra-ui/react";
 import AdminNavBar from "../components/AdminNavBar";
 import UserSignupPage from "./users/UserSignupPage";
 
@@ -17,6 +18,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard/:queueId" element={<QueueDashboard />} />
           <Route path="login" element={<Login />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="users/:queueId" element={<UserSignupPage />} />
---
+++ packages/frontend/src/views/dashboard/queueDashboard.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { Box, Button, Flex } from "@chakra-ui/react";
+
+export default function QueueDashboard() {
+  let { queueId } = useParams();
+  console.log(queueId);
+
+  return (
+    <>
+      <Flex flexDirection={"column"} justifyContent={"center"} my="3" mx="16">
+        {/* Header + Buttons */}
+        <Flex flexDirection={"row"} justifyContent={"space-between"}>
+          {/* <Box>
+            <h1>Queue Dashboard</h1>
+          </Box> */}
+          <Flex flexDirection={"column"}>
+            <Box>Queue Name</Box>
+            <Box>Users</Box>
+          </Flex>
+
+          <Flex flexDirection={"row"}>
+            <Button>Next User</Button>
+            <Button>Delete User</Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
