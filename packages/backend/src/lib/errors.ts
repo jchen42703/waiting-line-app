6641dc64781f22a214c119f559a087bc9ecd2678
Joseph Chen <jchen42703@gmail.com>
2022-02-25 15:05:32 -0500

    Created the custom HttpException

---
+++ packages/backend/src/lib/errors.ts
@@ -0,0 +1,15 @@
+/**
+ * To throw operational errors
+ *
+ * Usage:
+ *     next(new HTTPException(400, "is an operational error pls check input"))
+ */
+export class HttpException extends Error {
+  status: number;
+  message: string;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
