46a2059ab8b54aebbd453ab1e85dde459a7221dd
Joseph Chen <jchen42703@gmail.com>
2022-02-25 15:38:01 -0500

    Added error middleware

---
+++ packages/backend/src/middleware/error.ts
@@ -0,0 +1,38 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { HttpException } from "../lib/errors";
+import { stringifyReq } from "../lib/parse";
+import { logger } from "../lib/log";
+import { OperationalErrResp } from "@waiting-line-app/shared-dto/errors";
+
+/**
+ * Global uncaught error handler to prevent crash + catches operational errors
+ *
+ * @param error
+ * @param req
+ * @param res
+ * @param _
+ */
+const errorMiddleware: ErrorRequestHandler = (
+  error: HttpException | Error,
+  req: Request,
+  res: Response<OperationalErrResp, unknown>,
+  _: NextFunction,
+) => {
+  // just in-case we accidentally send a regular error instead of an HTTPException
+  const status = error instanceof HttpException ? error.status : 500;
+  // only log when server error
+  if (status >= 500) {
+    logger.info(`server error for req: ${stringifyReq(req)})}`);
+    logger.error(error);
+    return res.status(status).json({
+      message: "Something went wrong.",
+    });
+  }
+
+  const message = error.message || "Something went wrong";
+  return res.status(status).json({
+    message: message,
+  });
+};
+
+export default errorMiddleware;
