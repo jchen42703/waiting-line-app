3717c3e6ffc99d57364f0d1d3fb2b4e74e92dbd0
Joseph Chen <jchen42703@gmail.com>
2022-03-26 23:11:26 -0400

    [backend] Cleaned up the whitelist

--- packages/backend/src/middlewares/cookieValidator.ts
+++ packages/backend/src/middlewares/cookieValidator.ts
@@ -7,17 +7,12 @@ const cookieValidator = async (
   next: NextFunction,
 ) => {
   // Whitelisting urls that don't need to authenticated
-  const whitelist = [
-    "/api/queue/progress",
-    "/api/queue/join",
-    "/api/auth/login",
-    "/api/auth/logout",
-  ];
+  const whitelist = ["/api/queue/progress", "/api/queue/join", "/api/auth"];
 
   // req.url is the full requested url
   // (i.e. /api/queue/progress?queueId=db6bdf75-7ffd-4d68-ac54-74df2e2153fc&userId=4f1dab1b-028f-4233-ae31-2a84fc140758)
   for (let i = 0; i < whitelist.length; i++) {
-    if (req.url.includes(whitelist[i])) {
+    if (req.url.includes(whitelist[i]) || !req.url.includes("/api")) {
       return next();
     }
   }
