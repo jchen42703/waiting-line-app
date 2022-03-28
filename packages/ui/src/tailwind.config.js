fadfb325462a5bf27a6994d32698ce409cdbc62c
Joseph Chen <jchen42703@gmail.com>
2022-03-28 16:37:12 -0400

    [ui] Added typescript version of our UI

---
+++ packages/ui/src/tailwind.config.js
@@ -0,0 +1,6 @@
+// https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
