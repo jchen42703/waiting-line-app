adfdf5f3e5aa03ba4f26ae4ee332d8f25bd227fe
Joseph Chen <jchen42703@gmail.com>
2022-04-09 12:50:32 -0400

    [ui] added ability to conditionally render Footer gif

--- packages/ui/src/views/home/Home.tsx
+++ packages/ui/src/views/home/Home.tsx
@@ -1,21 +1,18 @@
-import {Fragment} from "react";
-import Navbar from "../../components/home/Navbar"
-import Hero from "../../components/home/Hero"
-import About from "../../components/home/About"
-import Team from "../../components/home/Team"
-import Footer from "../../components/home/Footer"
+import { Fragment } from "react";
+import Navbar from "../../components/home/Navbar";
+import Hero from "../../components/home/Hero";
+import About from "../../components/home/About";
+import Team from "../../components/home/Team";
+import Footer from "../../components/home/Footer";
 
-export default function Home(){
-
-  return(
+export default function Home() {
+  return (
     <Fragment>
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
       <Team></Team>
-      <Footer></Footer>
+      <Footer showGif={true}></Footer>
     </Fragment>
-  )
+  );
 }
-
-
