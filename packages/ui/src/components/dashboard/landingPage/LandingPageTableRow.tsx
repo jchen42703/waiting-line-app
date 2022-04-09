2877e7fd39064e92515119cc32feb6c33a89df6e
Joseph Chen <jchen42703@gmail.com>
2022-04-09 12:39:12 -0400

    [ui] Updated landing page table to contain more info

--- packages/ui/src/components/dashboard/landingPage/LandingPageTableRow.tsx
+++ packages/ui/src/components/dashboard/landingPage/LandingPageTableRow.tsx
@@ -1,22 +1,42 @@
 import { Tr, Td } from "@chakra-ui/react";
 import StatusCircle from "./StatusCircle";
+import { RepeatCycle } from "@lyne/shared-dto";
 
 interface LandingPageTableRowProps {
   queueName: string;
   numUsers: string;
   status: boolean;
+  timeCreated: number;
+  liveTime: number;
+  closeTime?: number;
+  repeatCycle?: RepeatCycle | null;
 }
 
 const LandingPageTableRow = ({
   queueName,
   numUsers,
+  timeCreated,
+  liveTime,
+  closeTime,
+  repeatCycle,
   status,
 }: LandingPageTableRowProps) => {
+  const createDate = new Date(timeCreated).toLocaleDateString();
+  const liveDate = new Date(liveTime).toLocaleDateString();
+  const closeDate = closeTime
+    ? new Date(closeTime).toLocaleDateString()
+    : "N/A";
+  const cycleMode = repeatCycle ? repeatCycle : "N/A";
+
   return (
     <>
       <Tr>
         <Td textAlign={"center"}>{queueName}</Td>
         <Td textAlign={"center"}>{numUsers}</Td>
+        <Td textAlign={"center"}>{createDate}</Td>
+        <Td textAlign={"center"}>{liveDate}</Td>
+        <Td textAlign={"center"}>{closeDate}</Td>
+        <Td textAlign={"center"}>{cycleMode}</Td>
         <Td>
           <StatusCircle status={status}></StatusCircle>
         </Td>
--- packages/ui/src/views/dashboard/dashboard.tsx
+++ packages/ui/src/views/dashboard/dashboard.tsx
@@ -8,7 +8,9 @@ import {
   Th,
   Tbody,
 } from "@chakra-ui/react";
+import { RepeatCycle } from "@lyne/shared-dto";
 import { Fragment } from "react";
+import Footer from "../../components/home/Footer";
 import AdminNavBar from "../../components/AdminNavBar";
 import LandingPageTableRow from "../../components/dashboard/landingPage/LandingPageTableRow";
 
@@ -24,12 +26,16 @@ export default function Dashboard() {
         paddingTop={"8"}
       >
         <Heading as="h1">Queue Manager Dashboard</Heading>
-        <TableContainer>
+        <TableContainer minHeight={"80vh"}>
           <Table>
             <Thead>
               <Tr>
                 <Th>Queue Name</Th>
-                <Th>Users</Th>
+                <Th>Number of Users</Th>
+                <Th>Date Created</Th>
+                <Th>Queue Live Time</Th>
+                <Th>Queue Close Time</Th>
+                <Th>Cycle</Th>
                 <Th>Queue Status</Th>
               </Tr>
             </Thead>
@@ -37,21 +43,26 @@ export default function Dashboard() {
               <LandingPageTableRow
                 queueName="dab"
                 numUsers="5"
+                timeCreated={100}
+                liveTime={200}
+                closeTime={300}
+                repeatCycle={"daily" as RepeatCycle}
                 status={true}
               ></LandingPageTableRow>
               <LandingPageTableRow
                 queueName="dab"
                 numUsers="5"
+                timeCreated={100}
+                liveTime={200}
+                closeTime={300}
+                repeatCycle={"daily" as RepeatCycle}
                 status={false}
               ></LandingPageTableRow>
             </Tbody>
           </Table>
         </TableContainer>
+        <Footer></Footer>
       </Flex>
-      {/* <main style={{ padding: "1rem 0" }}>
-      <h2>Dashboard</h2>
-      <button className="btn btn-primary">daisyUI Button</button>{" "}
-    </main> */}
     </Fragment>
   );
 }
