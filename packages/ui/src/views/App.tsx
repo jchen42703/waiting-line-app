1dff9d8b80a6ee766b7599f111fb27fff22ff84b
Joseph Chen <jchen42703@gmail.com>
2022-04-09 01:16:00 -0400

    [ui] added a theme

--- packages/ui/src/views/App.tsx
+++ packages/ui/src/views/App.tsx
@@ -1,7 +1,7 @@
 import "../styles/styles.scss";
 import "focus-visible/dist/focus-visible";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Home from "./home/Home";
 import Login from "./auth/Login";
 import Dashboard from "./dashboard/dashboard";
@@ -11,9 +11,21 @@ import UserSignupPage from "./users/UserSignupPage";
 import UserWaitingPage from "./users/UserWaitingPage";
 import AuthGuard from "../components/auth/AuthGuard";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      light: "#F4F1DE",
+      red: "#E07A5F",
+      navy: "#3D405B",
+      blue: "#81B29A",
+      peach: "#F2CC8F",
+    },
+  },
+});
+
 export default function App() {
   return (
-    <ChakraProvider resetCSS={true}>
+    <ChakraProvider resetCSS={true} theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
