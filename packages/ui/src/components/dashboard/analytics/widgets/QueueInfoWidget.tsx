bf93028dc97a3d5af9670645e723820fa27da37a
Benson <36316652+Jinb2@users.noreply.github.com>
2022-04-16 22:30:25 -0400

    Queue Info

--- packages/ui/src/components/dashboard/analytics/widgets/QueueInfoWidget.tsx
+++ packages/ui/src/components/dashboard/analytics/widgets/QueueInfoWidget.tsx
@@ -1,21 +1,8 @@
-import { Box, VStack, Heading, Text, Stack, Image } from "@chakra-ui/react";
-import { useState } from "react";
-// @ts-ignore
-import queueinfo from "../../../media/analytics/queueinfo.svg";
+import { Box } from "@chakra-ui/react";
 
 const QueueInfoWidget = () => {
-  const [info, setInfo] = useState([]);
-
   return (
-    <Box boxShadow="xs" rounded="lg" bg="white" height="100%" w="90%">
-      <VStack>
-        <Heading fontSize={"2xl"}>Queue Name</Heading>
-        {info.length == 0 ? <Image boxSize="150" src={queueinfo} /> : null}
-        <Stack w="70%">
-          <Text textAlign="center">description</Text>
-        </Stack>
-      </VStack>
-    </Box>
+    <Box boxShadow="xs" rounded="lg" bg="white" height="100%" w="90%"></Box>
   );
 };
 
