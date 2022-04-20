98e42f75e82e689703c52b96dda28382a6b88ebd
Benson <36316652+Jinb2@users.noreply.github.com>
2022-04-20 16:16:15 -0400

    "Make queue list a link"

--- packages/ui/src/components/dashboard/analytics/widgets/QueueListWidget.tsx
+++ packages/ui/src/components/dashboard/analytics/widgets/QueueListWidget.tsx
@@ -1,59 +1,23 @@
-import {
-  Box,
-  Heading,
-  VStack,
-  Table,
-  TableContainer,
-  Tr,
-  Thead,
-  Td,
-  Tbody,
-  Th,
-  Image,
-} from "@chakra-ui/react";
-
+import { Text, Box, Heading, VStack, Image, Link } from "@chakra-ui/react";
+import { Link as ReachLink } from "react-router-dom";
 // @ts-ignore
 import queuelist from "../../../media/analytics/queuelist.svg";
 
-const UsersTable = ({ queuedUsers }) => {
-  if (queuedUsers.length == 0) {
-    return <Image boxSize="150" pb="5%" src={queuelist}></Image>;
-  }
-
-  return (
-    <TableContainer overflowY="auto" maxHeight="250px">
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Phone Number</Th>
-            <Th isNumeric>Email</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {queuedUsers.map((user) => (
-            <Tr>
-              <Td>{user.name}</Td>
-              <Td>{user.phoneNumber} </Td>
-              <Td>{user.email}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  );
-};
-
 const QueueListWidget = ({ queuedUsers }) => {
   return (
-    <Box boxShadow="xs" rounded="lg" bg="white" height="100%" w="100%">
-      <VStack>
-        <Heading pt="5%" pb="5%" fontSize={"2xl"}>
-          Users
-        </Heading>
-        <UsersTable queuedUsers={queuedUsers}></UsersTable>
-      </VStack>
-    </Box>
+    <Link style={{ textDecoration: "none" }} as={ReachLink} to="/dashboard">
+      <Box boxShadow="xs" rounded="lg" bg="white" height="100%" w="100%">
+        <VStack>
+          <Heading pt="5%" pb="5%" fontSize={"2xl"}>
+            Manage your users
+          </Heading>
+          <Text fontSize={"lg"} fontWeight="bold">
+            Users : {queuedUsers.length}
+          </Text>
+          <Image boxSize="150" src={queuelist}></Image>
+        </VStack>
+      </Box>
+    </Link>
   );
 };
 
