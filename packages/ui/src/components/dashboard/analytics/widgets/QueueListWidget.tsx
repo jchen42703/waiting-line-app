ea69346d0dabe3463805827e5ac66afbf0813be4
Benson <36316652+Jinb2@users.noreply.github.com>
2022-04-20 16:35:10 -0400

    [ui] add link to users page for that queueid

--- packages/ui/src/components/dashboard/analytics/widgets/QueueListWidget.tsx
+++ packages/ui/src/components/dashboard/analytics/widgets/QueueListWidget.tsx
@@ -1,11 +1,16 @@
 import { Text, Box, Heading, VStack, Image, Link } from "@chakra-ui/react";
-import { Link as ReachLink } from "react-router-dom";
+import { Link as ReachLink, useParams } from "react-router-dom";
 // @ts-ignore
 import queuelist from "../../../media/analytics/queuelist.svg";
 
 const QueueListWidget = ({ queuedUsers }) => {
+  const { queueId } = useParams();
   return (
-    <Link style={{ textDecoration: "none" }} as={ReachLink} to="/dashboard">
+    <Link
+      style={{ textDecoration: "none" }}
+      as={ReachLink}
+      to={`/dashboard/users/${queueId}`}
+    >
       <Box boxShadow="xs" rounded="lg" bg="white" height="100%" w="100%">
         <VStack>
           <Heading pt="5%" pb="5%" fontSize={"2xl"}>
