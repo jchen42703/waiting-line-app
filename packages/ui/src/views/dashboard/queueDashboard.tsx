faa95abdc883c97ad58afa77d02e380450c374a5
Joseph Chen <jchen42703@gmail.com>
2022-04-20 16:21:08 -0400

    [ui] removed unnecessary imports in QueueDashboard

--- packages/ui/src/views/dashboard/queueDashboard.tsx
+++ packages/ui/src/views/dashboard/queueDashboard.tsx
@@ -1,21 +1,13 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import AdminNavBar from "../../components/AdminNavBar";
-import { getQueue } from "../../lib/services/queue.service";
-import { IQueue } from "@lyne/shared-dto";
-import BackButton from "../../components/BackButton";
-import { popUser } from "../../lib/services/user.service";
 import UserTableManager from "../../components/dashboard/userManager/UserTableManager";
 
 export default function QueueDashboard() {
   return (
     <>
       <AdminNavBar></AdminNavBar>
-
       <Flex flexDirection={"column"} justifyContent={"center"} my="3" mx="16">
         <UserTableManager></UserTableManager>
-        {/* <QueueTable deleteUserEnabled={deleteToggleState} queueId={queueId} /> */}
       </Flex>
     </>
   );
