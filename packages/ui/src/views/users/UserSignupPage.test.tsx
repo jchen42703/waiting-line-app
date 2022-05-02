9d045ca8dfbc80093f3a332050a6c11db9861794
Joseph Chen <jchen42703@gmail.com>
2022-05-01 20:19:40 -0400

    [ui] UserSignupPage test setup

---
+++ packages/ui/src/views/users/UserSignupPage.test.tsx
@@ -0,0 +1,26 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import UserSignupPage from "./UserSignupPage";
+
+// pay attention to write it at the top level of your file
+const mockedUsedNavigate = jest.fn();
+const mockedUseParams = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => mockedUsedNavigate,
+  useParams: () => mockedUseParams,
+}));
+
+describe("UserSignupPage", () => {
+  beforeEach(() => jest.resetAllMocks());
+  afterEach(() => cleanup());
+
+  it("should display a form", async () => {
+    render(<UserSignupPage />);
+    expect(
+      screen.getByText(/Please fill out this form to join queue/i),
+    ).toBeInTheDocument();
+
+    const joinButton = screen.getByRole("button", { name: "Join" });
+    expect(joinButton).toBeInTheDocument();
+  });
+});
